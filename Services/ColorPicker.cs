908455e2774fb7694fc0fef8dd3b7c4e1cb7993f
Dumas Valentin <valentin.dumas@viacesi.fr>
2017-12-18 00:02:51 +0100

    Add static ColorPicker to register Customs Colors

---
+++ Services/ColorPicker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gdk;
+
+namespace CesiWatch
+{
+	public static class ColorPicker
+	{
+		public static Color Red = new Gdk.Color(255, 0, 0);
+
+		public static Color DarkGray = new Gdk.Color(79, 79, 79); // MainWindow's Background Color
+
+		public static Color Gray = new Gdk.Color(100, 100, 100); // AreaMap's Background Color
+
+		public static Color Blue = new Gdk.Color(79, 202, 230);
+
+		public static Color Orange = new Gdk.Color(251, 148, 24);
+
+		public static Color Green = new Gdk.Color(153, 205, 87);
+	}
+}
