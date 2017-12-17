72f9042231be20a4572372b0b55c997b5ba63cbf
Dumas Valentin <valentin.dumas@viacesi.fr>
2017-12-17 20:00:03 +0100

    Refactor UIHandler Class

--- UIHandler.cs
+++ UIHandler.cs
@@ -4,56 +4,45 @@ namespace CesiWatch
 {
 	public class UIHandler
 	{
-		private MainWindow mainWindow_ = null;
-		private Gdk.GC graphicContextWindow_ = null;
-		private Gdk.GC graphicContextAreaMap_ = null;
+		private MainWindow mainWindow_ = null; // MainWindow's reference
 
-		Color red_ = new Gdk.Color(255, 0, 0);
-		Color darkGray_ = new Gdk.Color(79, 79, 79);
-		Color gray_ = new Gdk.Color(100, 100, 100);
+		private Gdk.GC graphicContextWindow_ = null; // MainWindow's Context
+
+		private Gdk.GC graphicContextAreaMap_ = null; // AreaMap's Context
+
+		Color red_ = new Gdk.Color(255, 0, 0); // Point's Foreground Color
+
+		Color darkGray_ = new Gdk.Color(79, 79, 79); // MainWindow's Background Color
+
+		Color gray_ = new Gdk.Color(100, 100, 100); // AreaMap's Background Color
 
 		public Gdk.GC GraphicContextWindow
 		{
-			get
-			{
-				return graphicContextWindow_;
-			}
-			set
-			{
-				graphicContextWindow_ = value;
-			}
+			get { return graphicContextWindow_; }
+			set { graphicContextWindow_ = value; }
 		}
 
 		public Gdk.GC GraphicContextAreaMap
 		{
-			get
-			{
-				return graphicContextAreaMap_;
-			}
-			set
-			{
-				graphicContextAreaMap_ = value;
-			}
+			get { return graphicContextAreaMap_; }
+			set { graphicContextAreaMap_ = value; }
 		}
 
 		public MainWindow MainWindow
-		{
-			get
-			{
-				return mainWindow_;
-			}
-			set
-			{
-				mainWindow_ = value;
-			}
+		{ 
+			get { return mainWindow_; }
+			set { mainWindow_ = value; }
 		}
 
 		public UIHandler(MainWindow mainWindow)
 		{
 			mainWindow_ = mainWindow;
+
 			graphicContextWindow_ = new Gdk.GC((Drawable)mainWindow_.GdkWindow);
+
 			graphicContextAreaMap_ = new Gdk.GC((Drawable)mainWindow_.DrawingAreaMap.GdkWindow);
 		}
+
 		public void Initialize()
 		{
 			LoadBackGrounds();
@@ -78,7 +67,6 @@ namespace CesiWatch
 
 		public void DrawMapPoint(int x, int y, int w = 4, int h = 4)
 		{
-			
 			mainWindow_.DrawingAreaMap.GdkWindow.DrawRectangle(GraphicContextAreaMap, true, x, y, w, h);
 		}
 	}
